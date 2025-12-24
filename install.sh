18e34110c47fcf12b1a54b2f881c56371ea4b0d0
MARCELO NASCIMENTO <148901953+Marcelo1408@users.noreply.github.com>
2025-12-23 23:08:57 -0300

    Format PHP installation command in install.sh

--- install.sh
+++ install.sh
@@ -194,23 +194,23 @@ install_php() {
     
     # Instalar PHP 8.2 e extensões
     apt install -y \
-        php8.2 \
-        php8.2-cli \
-        php8.2-fpm \
-        php8.2-mysql \
-        php8.2-curl \
-        php8.2-gd \
-        php8.2-mbstring \
-        php8.2-xml \
-        php8.2-zip \
-        php8.2-bcmath \
-        php8.2-intl \
-        php8.2-redis \
-        php8.2-soap \
-        php8.2-json \
-        php8.2-common \
-        php8.2-opcache \
-        libapache2-mod-php8.2
+    php8.2 \
+    php8.2-cli \
+    php8.2-fpm \
+    php8.2-mysql \
+    php8.2-curl \
+    php8.2-gd \
+    php8.2-mbstring \
+    php8.2-xml \
+    php8.2-zip \
+    php8.2-bcmath \
+    php8.2-intl \
+    php8.2-redis \
+    php8.2-soap \
+    php8.2-common \
+    php8.2-opcache \
+    libapache2-mod-php8.2
+
     
     # Configurar PHP
     PHP_INI="/etc/php/8.2/apache2/php.ini"
